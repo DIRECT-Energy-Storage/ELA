dace654a415437eabbe3cd4592d047812cf73e11
Ethan Wang <wfqethan@uw.edu>
2017-03-08 10:19:42 -0800

    initial commit

---
+++ ela/test_ela.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+import ela
+
+
+def test_get_latlon_from_zip():
+    test_df = pd.DataFrame()
+    return
+
+
+def test_get_state_from_zip():
+    test_df = pd.DataFrame()
+    return
+
+
+def test_get_closest_facility():
+    test_df = pd.DataFrame()
+    return
+
+
+def test_get_predicted_type():
+    test_df = pd.DataFrame()
+    return
+
+
+def test_get_state_breakdown():
+    test_df = pd.DataFrame()
+    return
+
+
+def test_get_energy_breakdwon():
+    test_df = pd.DataFrame()
+    return
+
+
+def test_split_data_by_state():
+    test_df = pd.DataFrame()
+    return
