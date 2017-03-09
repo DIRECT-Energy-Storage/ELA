b7d1e6d3969bee586f49cfaa131e963088e16b5a
Khushmeen <ksakloth@uw.edu>
2017-03-08 16:11:44 -0800

    some testing functions

--- ela/test_ela.py
+++ ela/test_ela.py
@@ -5,12 +5,18 @@ import ela
 
 
 def test_get_latlon_from_zip():
-    test_df = pd.DataFrame()
+    """Tests for checking the latitude-longitude for the entered zip code"""
+    assert ela.get_latlon_from_zip(98105)== (47.66377, -122.30118), "Zipcode entered incorrectly!"
+    assert type(ela.get_latlon_from_zip(81059))==tuple, "Lat-Lon is not in the expected format"
+    assert len(ela.get_latlon_from_zip(81059))==2 , "The length of the tuple is incorrect"
     return
 
 
 def test_get_state_from_zip():
-    test_df = pd.DataFrame()
+    """Test for checking the state for the entered zipcode"""
+    assert type(ela.get_state_from_zip(11206))==str,"Output is not a string"
+    assert ela.get_state_from_zip(98105)=='WA', "Zipcode does not match the state"
+    assert len(ela.get_state_from_zip(98105))==2 ,"The length of the string is incorrect"
     return
 
 
@@ -20,7 +26,7 @@ def test_get_closest_facility():
 
 
 def test_get_predicted_type():
-    test_df = pd.DataFrame()
+     assert type(ela.get_predicted_type(ela.get_latlon_from_zip(98105),'stor'))==str ,"Output is not a string"
     return
 
 
