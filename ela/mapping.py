c8645cf94a85fc2da80874daba14fe24ff6034df
Khushmeen <ksakloth@uw.edu>
2017-03-11 16:35:38 -0800

    more functions added

--- ela/mapping.py
+++ ela/mapping.py
@@ -61,3 +61,40 @@ def gen_statewise(state):
         else:
             pass
     return m
+
+IPython.display.Image(filename='Legend_Stor.png')
+
+def stor_statewise(state):
+    """
+    Given a state, it identifies the storage facilities of that
+    state and plots them in a color code based on the type of the
+    facility using folium as a mapping tool.
+
+    Parameters
+    ----------
+    state : str
+
+    Returns
+    -------
+    map
+
+    """
+
+    stor_df = ela.stor_data
+    stor_df = stor_df[stor_df.state==state]
+    m=folium.Map()
+    for index,rows in stor_df.iterrows():
+        f = folium.map.FeatureGroup()
+        if rows[5]=='Electro-mechanical':
+            f.add_child(folium.features.CircleMarker([rows[2],rows[3]],radius=3,fill_color='#f6cc0a')) #YELLOW
+            m.add_child(f)
+        elif rows[5]=='Electro-chemical':
+            f.add_child(folium.features.CircleMarker([rows[2],rows[3]],radius=3,fill_color='#2B90F5')) #BLUE
+            m.add_child(f)
+        elif rows[5]=='Pumped Hydro Storage':
+            f.add_child(folium.features.CircleMarker([rows[2],rows[3]],radius=3,fill_color='#b9341b')) #RED
+            m.add_child(f)
+        elif rows[5]=='Thermal Storage':
+            f.add_child(folium.features.CircleMarker([rows[2],rows[3]],radius=3,fill_color='#55b64e')) #GREEN
+            m.add_child(f)
+    return m
