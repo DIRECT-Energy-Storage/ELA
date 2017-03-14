e01d36abc3ea2f1853dae15cad72037addf61db3
David Beck <adurus@uw.edu>
2017-03-14 14:40:52 -0700

    unit tests for mapping funcitons updated

--- test_mapping.py
+++ test_mapping.py
@@ -1,5 +1,6 @@
 import ela
-import mapping
+from ela import *
+import ela.mapping as mapping
 import folium
 import pandas as pd
 import json
@@ -10,15 +11,23 @@ import IPython.display
 
 def test_geojson_to_df():
    assert ela.geojson_to_df(states).polygon[0] == ela.geojson_to_df(states).polygon[3], "Geometry types are not similar for given locations in Alabama and Arizona"
+   return
 
 
-def test_gen_statewise():
-    assert len(vars(ela.gen_statewise('WA')))==len(vars(ela.gen_statewise('NY'))), "The number of the items in the dictionaries not equal "
-    assert type(ela.gen_statewise('WA'))==type(ela.gen_statewise('WA')), "Error!, type of objects are not similar"
-    assert repr(ela.gen_statewise('AL'))== repr(ela.gen_statewise('WA')), "Representations of the objects are not similar"
+def test_prediction_map():
+    assert len(vars(ela.prediction_map(states,'gen')))==len(vars(ela.prediction_map(states, 'stor'))), "The number of the items in both the dictionaries not equal "
+    assert type(ela.prediction_map(states,'gen'))==type(ela.prediction_map(states,'stor')), "Error!, type of objects are not similar"
+    return
 
 
-def test_stor_statewise():
-    assert len(vars(ela.gen_statewise('WA')))==len(vars(ela.gen_statewise('NY'))), "The number of the items in the dictionaries not equal "
-    assert type(ela.gen_statewise('WA'))==type(ela.gen_statewise('WA')), "Error!, type of objects are not similar"
-    assert repr(ela.gen_statewise('AL'))== repr(ela.gen_statewise('WA')), "Representations of the objects are not similar"
+
+def test_facility_map():
+    assert len(vars(ela.facility_map('WA', 'gen')))==len(vars(ela.facility_map('NY', 'stor'))), "The number of the items in the dictionaries not equal "
+    assert type(ela.facility_map('WA', 'gen'))==type(ela.facility_map('WA','stor')), "Error!, type of objects are not similar"
+    return
+    
+
+def test_state_map():
+    assert len(vars(ela.state_map('WA')))==len(vars(ela.state_map('NY'))), "The number of the items in the dictionaries not equal "
+    assert type(ela.state_map('AZ'))==type(ela.state_map('AZ')), "Error!, type of objects are not similar"
+    return
