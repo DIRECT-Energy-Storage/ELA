d30140f8f0e547df1b16f640f1a30a41d67936af
Heidi Nelson <hdnelson@uw.edu>
2017-03-15 11:13:40 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(name='ELA',
+      version='0.1',
+      description='Energy by Location in America',
+      long_description='Class project for UW DIRECT',
+      url='https://github.com/DIRECT-Energy-Storage/ELA',
+      author='UW DIRECT students',
+      license='MIT',
+      packages=find_packages()
+      #install_requires=[],
+      #package_data={
+      #    'ela' : ['ela/data/']
+      #}
+
+)
