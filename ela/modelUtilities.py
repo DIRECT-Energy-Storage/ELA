a0224bdd4929fc73ff46b4bf63991d3026259d87
Heidi Nelson <hdnelson@uw.edu>
2017-03-15 18:45:53 -0700

    Added KNN map visualization function

--- ela/modelUtilities.py
+++ ela/modelUtilities.py
@@ -1,12 +1,14 @@
 """Functions for exploring and evaluating a K-nearest-neighbors model
-with different parameters and data sets"""
+with different parameters and data sets. These functions are not part
+of the main core of ELA and do not have comprehensive testing and error
+handling."""
 
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+import folium
 
 import ela
-
 
 gen = ela.gen_data
 stor = ela.stor_data
@@ -50,14 +52,17 @@ def predict_types(k, weights, train_df, test_df):
     Side Effects
     ------------
     Adds 'pred' column to both train_df and test_df, containing predicted
-    energy type for each location.
+    energy type for each location. Overwrites these columns if they already
+    exist.
 
     """
 
     clf = KNeighborsClassifier(n_neighbors=k, weights=weights)
     clf.fit(train_df[['lat', 'lon']], np.ravel(train_df.type))
-    train_df.loc[:, 'pred'] = clf.predict(train_df[['lat', 'lon']])
-    test_df.loc[:, 'pred'] = clf.predict(test_df[['lat', 'lon']])
+    train_df.is_copy = False
+    test_df.is_copy = False
+    train_df['pred'] = clf.predict(train_df[['lat', 'lon']])
+    test_df['pred'] = clf.predict(test_df[['lat', 'lon']])
 
 
 def count_pred_types(df):
@@ -178,13 +183,15 @@ def plot_knn_error(k_max, weights, train_df, test_df):
     train_error = result[:, 1]
     test_error = result[:, 2]
     plt.scatter(k, train_error, color='red', label='Training error')
-    plt.scatter(k, test_error, label='Testing error')
+    plt.scatter(k, test_error, color='blue', label='Testing error')
     plt.xlabel('K')
     plt.ylabel('Error rate')
     plt.ylim(0, 1)
+    plt.xlim(0, k_max + 1)
+    plt.legend()
 
 
-def geojson_predict_k(geo_df, gen_train, stor_train, k):
+def geojson_predict_k(geo_df, gen_train, stor_train, k, weights):
     """
     Predict generation and storage types for all features in a dataframe.
 
@@ -205,6 +212,8 @@ def geojson_predict_k(geo_df, gen_train, stor_train, k):
         Y values (predicted types) are the 'type' column.
     k : int
         Number of nearest-neighbors to consider in KNN model.
+    weights : string
+        Type of weighting to use for KNN model ('distance' or 'uniform')
 
     Returns
     -------
@@ -219,9 +228,9 @@ def geojson_predict_k(geo_df, gen_train, stor_train, k):
 
     """
 
-    gen_clf = KNeighborsClassifier(n_neighbors=k, weights='distance')
+    gen_clf = KNeighborsClassifier(n_neighbors=k, weights=weights)
     gen_clf.fit(gen_train[['lat', 'lon']], np.ravel(gen_train.type))
-    stor_clf = KNeighborsClassifier(n_neighbors=k, weights='distance')
+    stor_clf = KNeighborsClassifier(n_neighbors=k, weights=weights)
     stor_clf.fit(stor_train[['lat', 'lon']], np.ravel(stor_train.type))
     gens = []
     stors = []
@@ -231,3 +240,53 @@ def geojson_predict_k(geo_df, gen_train, stor_train, k):
         stors.append(stor_clf.predict(latlon)[0])
     geo_df['pred_gen'] = gens
     geo_df['pred_stor'] = stors
+
+
+def prediction_map_k(geoj, k, weights, gen_or_stor):
+    """
+    Create a Folium map layer with features colored by predicted energy type,
+    based on KNN with selected K and weighting type.
+
+    Parameters
+    ----------
+    geoj : GeoJSON object
+        Contains state or county boundaries
+    k : int
+        Number of nearest-neighbors to consider in KNN model.
+    weights : string
+        Type of weighting to use for KNN model ('distance' or 'uniform')
+    gen_or_stor : string, either 'gen' or 'stor'
+        Specify whether to map predicted generation or storage types.
+
+    Returns
+    -------
+    Folium GeoJson layer
+        Map layer showing the input geographic features colored according to
+        their predicted energy types, using KNN with the input K and weight
+
+    Raises
+    ------
+    ValueError if gen_or_stor is not either 'gen' or 'stor'.
+
+    Notes
+    -----
+    This is a variation on ela.mapping.prediction_map() which enables the KNN
+    weighting and K-value to be varied.
+
+    """
+
+    geo_df = ela.geojson_to_df(geoj)
+    ela.geojson_centers(geo_df)
+    geojson_predict_k(geo_df, ela.gen_data, ela.stor_data, k, weights)
+
+    if gen_or_stor == 'gen':
+        map_colors = ela.pred_gen_to_colors(geo_df)
+    elif gen_or_stor == 'stor':
+        map_colors = ela.pred_stor_to_colors(geo_df)
+    else:
+        raise ValueError("Enter either 'gen' or 'stor'.")
+
+    return folium.GeoJson(geoj,
+                          style_function=lambda feature: {
+                              'fillColor': map_colors[feature['id']],
+                              'weight': 0})
